ba53e0a0da8da177b353009b6820b9f955ad3d0a
Leonard Mark <leonardmarkdimayuga@gmail.com>
2025-01-20 00:22:15 +0800

    Ride List with todays_ride_events and pickup time sorting

--- wingz/app/endpoints/ride_endpoint.py
+++ wingz/app/endpoints/ride_endpoint.py
@@ -15,7 +15,14 @@ class RideAPI(BaseViewSet, GenericViewSet):
     queryset = Ride.objects.all()
 
     def list(self, request):
-        items = self.get_queryset()
+        sort_pickup_time = request.query_params.get("sort-pickup-time", "desc")
+
+        # Set default sort pickup_time to desc
+        sort_pickup = "-pickup_time"
+        if sort_pickup_time == "asc":
+            sort_pickup = "pickup_time"
+
+        items = self.get_queryset().order_by(sort_pickup)
         page = self.paginate_queryset(items)
         serializer = self.get_serializer(page, many=True)
         return self.get_paginated_response(serializer.data)
--- wingz/app/serializers/ride_serializer.py
+++ wingz/app/serializers/ride_serializer.py
@@ -1,3 +1,5 @@
+import datetime
+from django.utils import timezone
 from rest_framework import serializers
 
 from .ride_event_serializer import RideEventSerializer
@@ -5,10 +7,12 @@ from app.models import Ride
 
 
 class RideSerializer(serializers.ModelSerializer):
-    events = serializers.SerializerMethodField(read_only=True)
+    todays_ride_events = serializers.SerializerMethodField(read_only=True)
 
-    def get_events(self, obj: Ride):
-        events = obj.events.all()
+    def get_todays_ride_events(self, obj: Ride):
+        now_date = timezone.now()
+        pass_24hr = now_date - datetime.timedelta(hours=24)
+        events = obj.events.filter(created_at__range=(pass_24hr, now_date))
         serialize = RideEventSerializer(events, many=True)
         return serialize.data
 
