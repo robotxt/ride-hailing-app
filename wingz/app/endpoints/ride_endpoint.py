eb63472365b6980c91b5c24c261a460e39f67e17
Leonard Mark <leonardmarkdimayuga@gmail.com>
2025-01-20 12:10:13 +0800

    filter by distance

--- wingz/app/endpoints/ride_endpoint.py
+++ wingz/app/endpoints/ride_endpoint.py
@@ -1,3 +1,6 @@
+from django.contrib.gis.geos import Point
+from django.contrib.gis.db.models.functions import Distance
+from django.db.models import F
 from .viewsets import BaseViewSet
 from rest_framework import permissions
 from rest_framework.viewsets import GenericViewSet
@@ -16,13 +19,25 @@ class RideAPI(BaseViewSet, GenericViewSet):
 
     def list(self, request):
         sort_pickup_time = request.query_params.get("sort-pickup-time", "desc")
+        latitude = request.query_params.get("latitude")
+        longitude = request.query_params.get("longitude")
 
         # Set default sort pickup_time to desc
         sort_pickup = "-pickup_time"
         if sort_pickup_time == "asc":
             sort_pickup = "pickup_time"
 
-        items = self.get_queryset().order_by(sort_pickup)
+        items = self.get_queryset()
+        items = items.order_by(sort_pickup)
+        if latitude and longitude:
+            query_location = Point(longitude, latitude, srid=4326)
+            items = items.annotate(
+                location=Point(F("pickup_longitude"), F("pickup_latitude"), srid=4326),  # Construct a Point on-the-fly
+                distance=Distance(query_location, Point(F("pickup_longitude"), F("pickup_latitude"), srid=4326)),
+            ).order_by(
+                "distance"
+            )  # Sort: by closest distance
+
         page = self.paginate_queryset(items)
         serializer = self.get_serializer(page, many=True)
         return self.get_paginated_response(serializer.data)
